73c464514c3dbe9a6989890cd827be4ac67d43e3
Braden <bradent@outlook.com>
2022-01-13 16:51:48 -0700

    Added signalR notification support

---
+++ Application/Common/Notifications/INotificationService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Application.Common.Notifications
+{
+    public interface INotificationService
+    {
+        public Task Notify(INotification notification);
+    }
+}
