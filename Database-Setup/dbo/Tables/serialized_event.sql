56f4cbd479b89af6c97ead2633acd20ca1ba43ff
Braden <bradent@outlook.com>
2022-02-09 15:46:18 -0700

    remove deprecated database setup

--- Database-Setup/dbo/Tables/serialized_event.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[serialized_event]
-(
-	[aggregate_id] VARCHAR(50) NOT NULL PRIMARY KEY, 
-    [version] INT NOT NULL, 
-    [data] NVARCHAR(MAX) NOT NULL, 
-    [type] NVARCHAR(MAX) NOT NULL
-)
--- Database-Setup/dbo/Tables/speaker.sql
+++
@@ -1,5 +0,0 @@
-﻿CREATE TABLE [dbo].[speaker]
-(
-	[SerialNumber] VARCHAR(50) NOT NULL PRIMARY KEY, 
-    [Model] VARCHAR(50) NOT NULL
-)
